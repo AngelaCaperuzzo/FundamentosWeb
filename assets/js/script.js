364bb21b1e8b66deb22dfacdb3444eaeec0dc17e
caperuzzo.angela@gmail.com <caperuzzo.angela@gmail.com>
2021-04-22 17:33:54 -0300

    teste de bootstrap dia 22/04.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ case sensitive = reconhece letras maíusculas e minúsculas. */
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -21,6 +24,7 @@ let txtNome = document.querySelector('#txtNome')
     }else {
     txtNome.innerHTML = 'Nome válido'
     txtNome.style.color = 'blue'
+    nomeOk = true
     }
 }
 
@@ -33,5 +37,25 @@ function validaEmail(){
     }else{
         txtEmail.innerHTML = 'E-mail válido'
         txtEmail.style.color = 'blue'
+        emailOk = true
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if(assunto.value.length >=50){
+        txtAssunto.innerHTML =' texto muito grande, compátivel somente até 100 caracteres.'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    }else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+function enviar (){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert ('Mensagem enviada com sucesso! :D')
+    }else{
+        alert ('Por favor preencha os campos corretamente, para que possa ser enviado.')
     }
 }
